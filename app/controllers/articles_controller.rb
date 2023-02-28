976f7fb52f13b4563d44bf62acca53d04691317a
nitin kumar <nk378266@gmail.com>
2023-02-28 13:39:49 +0530

    till styling

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,8 @@
 class ArticlesController < ApplicationController
+
+	before_action :set_article, only: [:show, :edit, :update, :destroy]
+
 	def show
-		@article = Article.find(params[:id])
 	end
 
 	def index
@@ -14,7 +16,7 @@ class ArticlesController < ApplicationController
 	def create
 		# render plain: params[:article] 
 
-		@article = Article.new(params.require(:article).permit( :title, :description))
+		@article = Article.new(article_params)
 		@article.save
 		# render plain: @article.inspect
 		if @article.save
@@ -26,12 +28,10 @@ class ArticlesController < ApplicationController
 	end
 
 	def edit
-		@article = Article.find(params[:id])
 	end
 
 	def update
-		@article = Article.find(params[:id])
-		if @article.update(params.require(:article).permit(:title, :description))
+		if @article.update(article_params)
 			flash[:notice] = "updated succesfully"
 			redirect_to @article 
 		else
@@ -40,8 +40,17 @@ class ArticlesController < ApplicationController
 	end
 
 	def destroy
-		@article = Article.find(params[:id])
 		@article.destroy
-		redirect_to articles_path	
+		redirect_to articles_path
+	end
+
+ 	private
+
+ 	def set_article
+		@article = Article.find(params[:id])
+	end
+
+	def article_params
+		params.require(:article).permit(:title, :description)
 	end
 end
